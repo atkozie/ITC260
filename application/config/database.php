7be4b6fb0fad49838e2fb6750397a86bf2731976
atkozie <atkozie@gmail.com>
2017-04-18 16:03:39 -0700

    Add files via upload

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '###',
-	'username' => '###',
-	'password' => '###',
-	'database' => '###',
+	'hostname' => '####',
+	'username' => '####',
+	'password' => '####',
+	'database' => '####',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
